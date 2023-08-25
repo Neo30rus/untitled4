def2191cbd733b537a35aac2aeb3928cff92a662
alevur <alevure@yandex.ru>
2023-08-25 20:16:19 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.body;
+body.style.background = "red"
+
+const h1 = document.createElement("h1");
+h1.innerText = "Hello World!"
+document.body.appendChild(h1);
+
+
+const p1 = document.createElement("p1");
+p1.innerText = "123"
+p1.style.color = "blue"
+document.body.appendChild(p1)
+
+console.log(0.1+0.2);
